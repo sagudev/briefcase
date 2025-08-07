bd580c05c11053465a5d8d32c499a778103bb17f
sagudev <16504129+sagudev@users.noreply.github.com>
2025-08-07 17:32:40 +0200

    svg2canvas
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

---
+++ canvas2d/svg2canvas.js
@@ -0,0 +1,57 @@
+async function parse_svg(svg_text) {
+  const parser = new DOMParser();
+  const svgDoc = parser.parseFromString(svg_text, "image/svg+xml");
+  const paths = svgDoc.querySelectorAll("path");
+  return Array.from(paths).map((svg_path) => ({
+    path: new Path2D(svg_path.getAttribute("d")),
+    fill: svg_path.getAttribute("fill"),
+    stroke: svg_path.getAttribute("stroke"),
+    stroke_width: svg_path.getAttribute("stroke-width"),
+  }));
+}
+
+async function fill_svg_paths(ctx, svg, x, y, zoom = 1) {
+  ctx.save();
+  ctx.translate(x, y); // or center based on viewBox
+  ctx.scale(zoom, zoom);
+  for (const obj of svg) {
+    ctx.lineWidth = obj.stroke_width;
+    ctx.fillStyle = obj.fill;
+    ctx.strokeStyle = obj.stroke;
+    //if (obj.stroke) ctx.stroke(obj.path);
+    ctx.fill(obj.path);
+  }
+  ctx.restore();
+}
+
+async function draw_svg_paths(ctx, svg, x, y) {
+  ctx.save();
+  ctx.translate(x, y); // or center based on viewBox
+  for (const obj of svg) {
+    ctx.lineWidth = obj.stroke_width;
+    ctx.fillStyle = obj.fill;
+    ctx.strokeStyle = obj.stroke;
+    if (obj.stroke) ctx.stroke(obj.path);
+    ctx.fill(obj.path);
+  }
+  ctx.restore();
+}
+
+async function stroke_svg_paths(ctx, svg, x, y, zoom = 1) {
+  ctx.save();
+  ctx.translate(x, y); // or center based on viewBox
+  ctx.scale(zoom, zoom);
+  for (const obj of svg) {
+    ctx.lineWidth = obj.stroke_width;
+    ctx.strokeStyle = obj.fill;
+    if (obj.stroke) ctx.strokeStyle = obj.stroke;
+    ctx.stroke(obj.path);
+  }
+  ctx.restore();
+}
+
+async function load_svg(url) {
+  return await fetch(url)
+    .then((response) => response.text())
+    .then(parse_svg);
+}
